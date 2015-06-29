745f2ce79f050dff89c1024f02a18ad4b19cb91b
fab_lyd <fab_lyd@126.com>
2015-06-29 18:12:41 +0800

    对于大文件，采用svg格式显示。

--- GeneratePreviewForURL.m
+++ GeneratePreviewForURL.m
@@ -3,6 +3,8 @@
 #include <QuickLook/QuickLook.h>
 #include <Foundation/Foundation.h>
 #include <AppKit/AppKit.h>
+#import <WebKit/WebKit.h>
+#import <Cocoa/Cocoa.h>
 #include "Dot.h"
 
 /* -----------------------------------------------------------------------------
@@ -14,14 +16,47 @@
 
 OSStatus GeneratePreviewForURL_with_svg(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options);
 OSStatus GeneratePreviewForURL_with_img(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options);
+OSStatus GeneratePreviewForURL_with_svg_using_web(void *thisInterface, QLPreviewRequestRef preview,
+                                                  CFURLRef url, CFStringRef contentTypeUTI,
+                                                  CFDictionaryRef options);
+
+static bool IsBigEnough(const char *szFile)
+{
+    FILE *fp = fopen(szFile, "rb");
+    if(fp)
+    {
+        fseek(fp, 0, SEEK_END);
+        return ftell(fp) > (15 * 1024);
+    }
+    return false;
+}
 
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
-#if 1
-    return GeneratePreviewForURL_with_svg(thisInterface, preview, url, contentTypeUTI, options);
-#else
-    return GeneratePreviewForURL_with_img(thisInterface, preview, url, contentTypeUTI, options);
-#endif
+    int nType = 0;
+    nType = 1;
+//    nType = 2;
+//    nType = 3;
+    
+    NSString *path = [(__bridge NSURL *)url path];
+    if(IsBigEnough([path cStringUsingEncoding:NSUTF8StringEncoding]))
+    {
+        nType = 2;
+    }
+    
+    if(nType == 1)
+    {
+        return GeneratePreviewForURL_with_img(thisInterface, preview, url, contentTypeUTI, options);
+    }
+    else if(nType == 2)
+    {
+        return GeneratePreviewForURL_with_svg(thisInterface, preview, url, contentTypeUTI, options);
+    }
+    else if(nType == 3)
+    {
+        return GeneratePreviewForURL_with_svg_using_web(thisInterface, preview, url, contentTypeUTI, options);
+    }
+    return noErr;
 }
 OSStatus GeneratePreviewForURL_with_img(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
@@ -42,13 +77,6 @@ OSStatus GeneratePreviewForURL_with_img(void *thisInterface, QLPreviewRequestRef
             CGImageRef image = CGImageCreateWithPNGDataProvider(imgDataProvider, NULL, true, kCGRenderingIntentDefault);
 			
             CGContextDrawImage(cgContext,CGRectMake(0, 0, imageForSize.size.width, imageForSize.size.height), image);
-//			CGContextMoveToPoint(cgContext, 10, 10);
-//            CGContextAddLineToPoint(cgContext, 0, -300);
-//            CGRect rc = {{0, 0,}, {10, 30}};
-//            CGContextFillEllipseInRect(cgContext, rc);
-            
-//            CGContextShowTextAtPoint(cgContext, 10, 10, "some string", 11);
-            
             QLPreviewRequestFlushContext(preview, cgContext);
             
             CFRelease(cgContext);
@@ -59,7 +87,130 @@ OSStatus GeneratePreviewForURL_with_img(void *thisInterface, QLPreviewRequestRef
 	
 	return noErr;
 }
-	
+static void WriteSthToFile(const char *szFile, const char *szText)
+{
+    FILE *fp = fopen(szFile, "wb");
+    if(fp)
+    {
+        while(fp)
+        {
+            if(!szText)
+            {
+                break;
+            }
+            int nLen = strlen(szText);
+            fwrite(szText, 1, nLen, fp);
+            break;
+        }while(false);
+        fclose(fp);
+    }
+}
+static void LogToFile(const char *szLog)
+{
+    const char *szFile = "/Volumes/Macintosh-HD/Test.localized/TrySomething/mylisp/dot/debug_log.txt";
+    FILE *fp = fopen(szFile, "ab");
+    if(!fp)
+    {
+        fp = fopen(szFile, "wb");
+    }
+    if(fp)
+    {
+        fwrite(szLog, 1, strlen(szLog), fp);
+        fclose(fp);
+    }
+}
+#define LOG_FILE_LINE(log) do{ \
+    char szBuf[1000]; \
+    sprintf(szBuf, "%s (%d): %s\n", __FILE__, __LINE__, log); \
+    LogToFile(szBuf); \
+    }while(false)
+
+OSStatus GeneratePreviewForURL_with_svg_using_web(void *thisInterface, QLPreviewRequestRef preview,
+                               CFURLRef url, CFStringRef contentTypeUTI,
+                               CFDictionaryRef options)
+{
+    NSData *imageData = [Dot dataFromDotFile: (__bridge NSURL *)url format:@"-Tsvg"];
+    NSData *data = NULL;
+    if (imageData)
+    {
+        NSString *svg = [[NSString alloc] initWithData:imageData encoding:NSUTF8StringEncoding];
+        
+        NSMutableString *html = [[NSMutableString alloc] init];
+        [html appendString:@"<html>"];
+        [html appendString:@"<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />"];
+        [html appendString:@"<head></head><body><h1>Dot SVG preview with web view</h1>"];
+        
+        [html appendString:svg];
+        
+        [html appendString:@"</body></html>"];
+        data = [html dataUsingEncoding:NSUTF8StringEncoding];
+        
+        const char *szText = [html cStringUsingEncoding:NSUTF8StringEncoding];
+        WriteSthToFile("/Volumes/Macintosh-HD/Test.localized/TrySomething/mylisp/dot/some.html", szText);
+    }
+    LogToFile("\r\n\r\n*********************************\r\n");
+    if (data) {
+        LOG_FILE_LINE("data is ok");
+        
+        CGSize maxSize = NSMakeSize(60, 80);
+        double w = 60;
+        double h = 80;
+        NSRect viewRect = NSMakeRect(0.0, 0.0, w, h);
+        float scale = maxSize.height / h;
+        NSSize scaleSize = NSMakeSize(scale, scale);
+        CGSize thumbSize = NSSizeToCGSize(
+                                          NSMakeSize((maxSize.width * (w / h)),
+                                                     maxSize.height));
+        LOG_FILE_LINE("before initWithFrame");
+        WebView* webView = [[WebView alloc] initWithFrame: viewRect];
+        LOG_FILE_LINE("");
+        [webView scaleUnitSquareToSize: scaleSize];
+        LOG_FILE_LINE("");
+        [[[webView mainFrame] frameView] setAllowsScrolling:NO];
+        LOG_FILE_LINE("");
+        [[webView mainFrame] loadData: data
+                             MIMEType: @"text/html"
+                     textEncodingName: @"utf-8"
+                              baseURL: nil];
+        
+        LOG_FILE_LINE("befor whild");
+        while([webView isLoading]) {
+            CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0, true);
+        }
+        LOG_FILE_LINE("before display");
+        
+        [webView display];
+        LOG_FILE_LINE("before create context");
+        
+        CGContextRef context =
+        QLPreviewRequestCreateContext(preview, thumbSize, false, NULL);
+        
+        if (context) {
+            LOG_FILE_LINE("context is ok");
+            NSGraphicsContext* nsContext =
+            [NSGraphicsContext
+             graphicsContextWithGraphicsPort: (void*) context
+													flipped: [webView isFlipped]];
+            
+            [webView displayRectIgnoringOpacity: [webView bounds]
+                                      inContext: nsContext];
+            
+            QLPreviewRequestFlushContext(preview, context);
+            
+            CFRelease(context);
+            LOG_FILE_LINE("");
+        }
+        else
+        {
+            LOG_FILE_LINE("context is null");
+        }
+    }
+    else
+    {
+        LOG_FILE_LINE("data is null");
+    }
+    return noErr;
+}
 OSStatus GeneratePreviewForURL_with_svg(void *thisInterface, QLPreviewRequestRef preview,
                                CFURLRef url, CFStringRef contentTypeUTI,
                                CFDictionaryRef options)
@@ -78,7 +229,8 @@ OSStatus GeneratePreviewForURL_with_svg(void *thisInterface, QLPreviewRequestRef
         
         [html appendString:@"</body></html>"];
         
-        CFDictionaryRef properties = (__bridge CFDictionaryRef)@{(NSString *) kQLPreviewPropertyWidthKey: @500, (NSString *)kQLPreviewPropertyWidthKey: @500};
+        CFDictionaryRef properties = (__bridge CFDictionaryRef) [NSDictionary dictionary];
+//        CFDictionaryRef properties = (__bridge CFDictionaryRef)@{(NSString *) kQLPreviewPropertyWidthKey: @50, (NSString *)kQLPreviewPropertyWidthKey: @50};
         QLPreviewRequestSetDataRepresentation(preview,
                                               (__bridge CFDataRef)[html dataUsingEncoding:NSUTF8StringEncoding],
                                               kUTTypeHTML,
--- GenerateThumbnailForURL.m
+++ GenerateThumbnailForURL.m
@@ -3,8 +3,9 @@
 #include <QuickLook/QuickLook.h>
 #include <Foundation/Foundation.h>
 #include <AppKit/AppKit.h>
+#import <WebKit/WebKit.h>
 #include "Dot.h"
-
+#include <stdio.h>
 /* -----------------------------------------------------------------------------
     Generate a thumbnail for file
 
@@ -13,8 +14,83 @@
 
 OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize)
 {
-//    #warning To complete your generator please implement the function GenerateThumbnailForURL in GenerateThumbnailForURL.c
-
+#if 0
+    NSData *imageData = [Dot dataFromDotFile: (__bridge NSURL *)url format:@"-Tsvg"];
+    NSData *data = NULL;
+    if (imageData)
+    {
+        NSString *svg = [[NSString alloc] initWithData:imageData encoding:NSUTF8StringEncoding];
+        
+        NSMutableString *html = [[NSMutableString alloc] init];
+        [html appendString:@"<html>"];
+        [html appendString:@"<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />"];
+        [html appendString:@"<head></head><body><h1>Dot SVG preview</h1>"];
+        
+        [html appendString:svg];
+        
+        [html appendString:@"</body></html>"];
+        data = [html dataUsingEncoding:NSUTF8StringEncoding];
+        
+        FILE *fp = fopen("/Volumes/Macintosh-HD/Test.localized/TrySomething/mylisp/dot/some.html", "wb");
+        if(fp)
+        {
+            while(fp)
+            {
+                const char *szText = [html cStringUsingEncoding:NSUTF8StringEncoding];
+                if(!szText)
+                {
+                    break;
+                }
+                int nLen = strlen(szText);
+                fwrite(szText, 1, nLen, fp);
+                break;
+            }while(false);
+            fclose(fp);
+        }
+    }
+    
+    if (data) {
+        double w = 600;
+        double h = 800;
+        NSRect viewRect = NSMakeRect(0.0, 0.0, w, h);
+        float scale = maxSize.height / 800.0;
+        NSSize scaleSize = NSMakeSize(scale, scale);
+        CGSize thumbSize = NSSizeToCGSize(
+                                          NSMakeSize((maxSize.width * (w / h)),
+                                                     maxSize.height));
+        
+        WebView* webView = [[WebView alloc] initWithFrame: viewRect];
+        [webView scaleUnitSquareToSize: scaleSize];
+        [[[webView mainFrame] frameView] setAllowsScrolling:NO];
+        [[webView mainFrame] loadData: data
+                             MIMEType: @"text/html"
+                     textEncodingName: @"utf-8"
+                              baseURL: nil];
+        
+        while([webView isLoading]) {
+            CFRunLoopRunInMode(kCFRunLoopDefaultMode, 0, true);
+        }
+        
+        [webView display];
+        
+        CGContextRef context =
+        QLThumbnailRequestCreateContext(thumbnail, thumbSize, false, NULL);
+        
+        if (context) {
+            NSGraphicsContext* nsContext =
+            [NSGraphicsContext
+             graphicsContextWithGraphicsPort: (void*) context
+													flipped: [webView isFlipped]];
+            
+            [webView displayRectIgnoringOpacity: [webView bounds]
+                                      inContext: nsContext];
+            
+            QLThumbnailRequestFlushContext(thumbnail, context);
+            
+            CFRelease(context);
+        }
+    }
+#endif
     return noErr;
 }
 
